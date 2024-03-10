1227ede70a4659e140a4325c83d1279c6c89c401
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2024-03-10 16:57:53 +0530

    second commit

--- src/ImageSlider/ImageSlide.jsx
+++ src/ImageSlider/ImageSlide.jsx
@@ -1,40 +1,77 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { BsArrowLeftCircleFill , BsArrowRightCircleFill} from "react-icons/bs";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./Style.css";
 function ImageSlide() {
-  const [image, setImage] = useState();
+  const [image, setImage] = useState([]);
+  const [cureenrSlide, setCureenrSlide] = useState(0);
   useEffect(() => {
     const getData = async () => {
       const url =
-        "https://api.unsplash.com/search/photos?page=1&query=office&client_id=tZqDJedwfME67dNXAFrY_LgQVSG4gYpuZcgW1R664C4";
+        "https://api.unsplash.com/search/photos?page=1&query=products&client_id=tZqDJedwfME67dNXAFrY_LgQVSG4gYpuZcgW1R664C4";
       const responce = await axios.get(url);
-      const data = await responce.data.results.slice(0, 5);
+      const data = await responce.data.results.slice(0, 10);
       console.log(data);
       setImage(data);
     };
     getData();
   }, []);
-
+  const hanldePrevious = () => {
+    setCureenrSlide(cureenrSlide === 0 ? image.length - 1 : cureenrSlide - 1);
+  };
+  const handleNext = () => {
+    setCureenrSlide(cureenrSlide === image.length - 1 ? 0 : cureenrSlide + 1);
+  };
   return (
-    <div>
-      <div className="main w-[600px] relative h-[450px] flex gap-2  ">
-        <BsArrowLeftCircleFill className="left-arrow" />
+    <div className=" h-[100vh] w-full flex justify-center items-center">
+      <div className="main w-[600px] relative h-[450px] flex gap-2 items-center ">
+        <BsArrowLeftCircleFill
+          onClick={hanldePrevious}
+          className="left-arrow arrow"
+        />
         {image ? (
-          image.map((items) => (
+          image.map((items, index) => (
             <img
               key={items.id}
-              className="current-image rounded-lg shadow-lg shadow-slate-800 w-full h-full"
+              className={
+                cureenrSlide === index
+                  ? "current-image rounded-lg shadow-lg shadow-slate-800 w-full h-full   bg-contain"
+                  : "current-image rounded-lg shadow-lg shadow-slate-800 w-full h-full hidden"
+              }
               src={items.urls.raw}
-              alt=""
+              alt={items.alt_description}
             />
           ))
-
         ) : (
           <h1 className="font-bold text-4xl h-[100vh] w-full flex justify-center items-center">
             loading data...
           </h1>
         )}
-        <BsArrowRightCircleFill className="right-arrow"/>
+        <BsArrowRightCircleFill
+          onClick={handleNext}
+          className="right-arrow arrow"
+        />
+
+        <span className="circle-indicator">
+          {image && image.length
+            ? image.map((_, index) => {
+                return (
+                  <button
+                    key={index}
+                    className={
+                      cureenrSlide === index
+                        ? "current-indicator"
+                        : "current-indicator bg-gray-600 text-white"
+                    }
+                    onClick={() => setCureenrSlide(index)}
+                  >
+                    {" "}
+                    {index}
+                  </button>
+                );
+              })
+            : ""}
+        </span>
       </div>
     </div>
   );
